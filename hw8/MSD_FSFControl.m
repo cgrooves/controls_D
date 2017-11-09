a34bcff521643482e16a60824ce5d01470adad68
cgrooves <caleb.j.groves@gmail.com>
2017-11-09 10:16:31 -0700

    Implemented saturation on full state feedback controller

--- hw8/MSD_FSFControl.m
+++ hw8/MSD_FSFControl.m
@@ -6,6 +6,7 @@ classdef MSD_FSFControl < handle
         
         beta
         Ts
+        limit
         
         ydot_d1
         y_d1
@@ -19,6 +20,7 @@ classdef MSD_FSFControl < handle
             
             self.beta = (2*P.tau - P.Ts)/(2*P.tau + P.Ts); % dirty derivative gain
             self.Ts = P.Ts;
+            self.limit = P.sat_limit;
             
             self.y_d1 = 0;
             self.ydot_d1 = 0;
@@ -30,13 +32,26 @@ classdef MSD_FSFControl < handle
             x(1) = y;
             x(2) = self.beta*self.ydot_d1 + 1/self.Ts*(1-self.beta)*(y - self.y_d1);
             
-            u = self.kr*r - self.K*x; % calculated input   
+            u = self.saturate(self.kr*r - self.K*x); % calculated input 
+            
             
             % set old values to current ones
             self.y_d1 = x(1);
             self.ydot_d1 = x(2);
             
         end
-        %----------------------------------        
+        %----------------------------------     
+        function u = saturate(self,uin)
+            
+            if uin < self.limit(1)
+                u = self.limit(1);
+            elseif uin > self.limit(2)
+                u = self.limit(2);
+            else
+                u = uin;
+            end
+            
+        end
+        %---------------------------------
     end  
 end
